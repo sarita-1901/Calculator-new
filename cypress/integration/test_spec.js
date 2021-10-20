496308b27b5f460e65967bfe4c0d2b3c7c676928
Sarita Kalyani <sarita.kalyani@db.com>
2021-10-20 11:21:04 +0530

    Final version

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -8,25 +8,79 @@ describe('My First Test', () => {
 // my actual first test - does the page load
 describe('The main page loads', () => {
   it('successfully loads', () => {
-    cy.visit('http://127.0.0.1:5500/index.html')
+    cy.visit('http://127.0.0.1:5501/index.html')
   }) 
 })
 
 
 // testing basic operations
-describe('Basic Calculator Operations', () => {
-  it('checks 7 + 2 equals 9', () => {
+describe('Basic Calculator Operation Add', () => {
+  it('checks 9 + 7 equals 16', () => {
     // Arrange
-    cy.visit('http://127.0.0.1:5500/index.html')
+    cy.visit('http://127.0.0.1:5501/index.html')
 
     // Act
-    cy.get("[data-cy=seven]").click();
-    cy.get(".calculator__operator--plus").click();
-    cy.get(".calculator__number--two").click();
-    cy.get(".calculator__operator--equals").click();
+    cy.get(".num-9").click();
+    cy.get(".operplus").click();
+    cy.get(".num-7").click();
+    cy.get(".operequal").click();
 
     // Assert
-    cy.get(".calculator__screen").should("have.value", "9");
+    cy.get(".calc-answer1").should("have.text", "16");
+
+    
+  }) 
+})
+
+describe('Basic Calculator Operation Minus', () => {
+  it('checks 9 - 7 equals 2', () => {
+    // Arrange
+    cy.visit('http://127.0.0.1:5501/index.html')
+
+    // Act
+    cy.get(".num-9").click();
+    cy.get(".operminus").click();
+    cy.get(".num-7").click();
+    cy.get(".operequal").click();
+
+    // Assert
+    cy.get(".calc-answer1").should("have.text", "2");
+
+  }) 
+})
+
+describe('Basic Calculator Operation Multiply', () => {
+  it('checks 9 * 7 equals 63', () => {
+    // Arrange
+    cy.visit('http://127.0.0.1:5501/index.html')
+
+    // Act
+    cy.get(".num-9").click();
+    cy.get(".opermultiply").click();
+    cy.get(".num-7").click();
+    cy.get(".operequal").click();
+
+    // Assert
+    cy.get(".calc-answer1").should("have.text", "63");
+    
+  }) 
+})
+
+describe('Basic Calculator Operation Divide', () => {
+  it('checks 9 divides 7 equals ', () => {
+    // Arrange
+    cy.visit('http://127.0.0.1:5501/index.html')
+
+    // Act
+    cy.get(".num-9").click();
+    cy.get(".operdivide").click();
+    cy.get(".num-7").click();
+    cy.get(".operequal").click();
+
+    // Assert
+    cy.get(".calc-answer1").should("have.text", "1.2857142857");
+
+  
   }) 
 })
 
